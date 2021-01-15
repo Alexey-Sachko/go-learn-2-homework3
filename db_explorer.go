c140c930dd52e3ea20f52ffe3c479172e021419e
Alexey Sachko <asachko@gmini.tech>
2021-01-15 12:31:29 +0500

    added columns fetching

--- db_explorer.go
+++ db_explorer.go
@@ -10,40 +10,87 @@ import (
 // обращаю ваше внимание - в этом задании запрещены глобальные переменные
 
 type Handler struct {
-	DB *sql.DB
+	DB     *sql.DB
+	tables []Table
+}
+
+type TableColumn struct {
+	Name    string
+	Type    string
+	Null    string
+	Key     string
+	Default interface{}
+	Extra   interface{}
+}
+
+type Table struct {
+	Name    string
+	Columns []TableColumn
 }
 
 func NewDbExplorer(db *sql.DB) (http.Handler, error) {
-	handler := Handler{DB: db}
+	tables, err := getTables(db)
+	if err != nil {
+		return nil, err
+	}
+
+	handler := Handler{DB: db, tables: tables}
 
 	mux := &http.ServeMux{}
-	mux.HandleFunc("/", method(http.MethodGet, handler.getTables))
+	mux.HandleFunc("/", method(http.MethodGet, handler.GetTables))
 	return mux, nil
 }
 
-func (h *Handler) getTables(w http.ResponseWriter, r *http.Request) {
-	rows, err := h.DB.Query("SHOW TABLES FROM golang")
+func getTables(db *sql.DB) ([]Table, error) {
+	tables := []Table{}
+
+	rows, err := db.Query("SHOW TABLES FROM golang")
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		writeErr(w,err)
-		return
+		return tables, err
 	}
 
-	tables := []string{}
-
 	for rows.Next() {
-		var table string
-		err := rows.Scan(&table)
+		table := Table{}
+		err := rows.Scan(&table.Name)
 		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			writeErr(w,err)
-			return
+			return tables, err
 		}
-		
+
+		table.Columns, err = getColumns(db, table.Name)
+		if err != nil {
+			return tables, err
+		}
+
 		tables = append(tables, table)
 	}
 
-	writeJSON(w, tables)
+	return tables, nil
+}
+
+func getColumns(db *sql.DB, tableName string) ([]TableColumn, error) {
+	columns := []TableColumn{}
+
+	rows, err := db.Query("SHOW COLUMNS FROM " + tableName)
+	if err != nil {
+		return columns, err
+	}
+
+	for rows.Next() {
+		col := TableColumn{}
+		err := rows.Scan(&col.Name, &col.Type, &col.Null, &col.Key, &col.Default,&col.Extra)
+		if err != nil {
+			return columns, err
+		}
+
+		columns = append(columns, col)
+	}
+
+	return columns, nil
+}
+
+func (h *Handler) GetTables(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	writeJSON(w, h.tables)
 }
 
 // utils
@@ -52,7 +99,7 @@ func writeErr(w http.ResponseWriter, err error) {
 }
 
 func writeJSON(w http.ResponseWriter, d interface{}) {
-	b, err := json.Marshal(d)
+	b, err := json.MarshalIndent(d, "", "  ")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		writeErr(w, err)
